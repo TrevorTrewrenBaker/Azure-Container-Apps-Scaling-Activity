839cc520947ffdf3e959e04f3b070762bbd5f4f4
Trevor Baker <trevor-baker@live.com.au>
2026-06-13 17:53:36 +1000

    TASK-01: Set-up the container, creating a script which handles the docker container, deploying to azure & setting up necessary resources. Configure the nextjs application to run azure

--- azure-scaling-lab/next.config.ts
+++ azure-scaling-lab/next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  // This enables the 'standalone' output
+  output: 'standalone',
+  
+  // Optional: Ensure images work if you host on ACA with external domains
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
